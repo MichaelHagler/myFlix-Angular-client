333a17d4bff602a1c92ac48f4527aa4c1202f2b1
MichaelHagler <67695612+MichaelHagler@users.noreply.github.com>
2023-05-31 19:59:08 -0700

    Update fetch-api-data.service.ts

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -28,11 +28,7 @@ export class FetchApiDataService {
     );
   }
 
-  // Making the api call for all movies endpoint
-  /**
-   * Gets all the movies from the api
-   * @returns  
-   */
+  //Making the api call for all movies endpoint
   getAllMovies(): Observable<IMovies[]> {
     const token = localStorage.getItem('token');
     return this.http
@@ -45,11 +41,6 @@ export class FetchApiDataService {
   }
 
   // Making the api call for one movie
-  /**
-   * gets a pacific movie by title
-   * @param title 
-   * @returns 
-   */
   getMovie(title: string): Observable<IMovies> {
     const token = localStorage.getItem("token");
     return this.http.get<IMovies>(`${apiUrl}/movies/${title}`, {headers: new HttpHeaders({
@@ -57,11 +48,7 @@ export class FetchApiDataService {
       })}).pipe(map(this.extractResponseData), catchError(this.handleError));
   }
 
-  /**
-   * gets a director by name. users can view info about the director
-   * @param directorName 
-   * @returns 
-   */
+  
   getDirector(directorName: string): Observable<any> {
     const token = localStorage.getItem("token");
     return this.http.get(apiUrl + "movies/directors", {headers: new HttpHeaders({
@@ -69,12 +56,7 @@ export class FetchApiDataService {
       })}).pipe(map(this.extractResponseData), catchError(this.handleError));
   }
 
-  /**
-   * Gets info about the genre of the movie. Name of the genre and a Description of the genre
-   * @param Name 
-   * @param Description 
-   * @returns 
-   */
+  
   getGenre(Name: string, Description: string): Observable<IMovies> {
     const token = localStorage.getItem("token");
     return this.http.get<IMovies>(`${apiUrl}/movies/genre/`, {headers: new HttpHeaders({
@@ -100,11 +82,6 @@ export class FetchApiDataService {
   // }
   //
 
-  /**
-   * Adds movie to user's favorite movies list
-   * @param movieId 
-   * @returns 
-   */
   addFavoriteMovie(movieId: string): Observable<any> {
     const token = localStorage.getItem("token");
     const localStorageUser = localStorage.getItem("user");
@@ -116,11 +93,6 @@ export class FetchApiDataService {
       .pipe(map(this.extractResponseData), catchError(this.handleError));
   }
 
-  /**
-   * allows users to edit their profiles
-   * @param updatedUser 
-   * @returns 
-   */
   editUser(updatedUser: any): Observable<any> {
     const token = localStorage.getItem("token");
     return this.http.put(apiUrl + "users/username", updatedUser, {headers: new HttpHeaders({
@@ -128,10 +100,6 @@ export class FetchApiDataService {
       })}).pipe(map(this.extractResponseData), catchError(this.handleError));
   }
 
-  /**
-   * deletes user profile
-   * @returns 
-   */
   deleteUser(): Observable<any> {
     const token = localStorage.getItem("token");
     return this.http.delete(apiUrl + "users/username", {headers: new HttpHeaders({
@@ -139,11 +107,6 @@ export class FetchApiDataService {
       })}).pipe(map(this.extractResponseData), catchError(this.handleError));
   }
 
-  /**
-   * removes a movie that is in a user's  favorite movie list
-   * @param movieId 
-   * @returns 
-   */
   removeFavoriteMovie(movieId: string): Observable<any> {
     const token = localStorage.getItem("token");
     const localStorageUser = localStorage.getItem("user");
